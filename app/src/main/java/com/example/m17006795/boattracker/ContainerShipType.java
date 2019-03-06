7a214fda7ee86cc46913aa8b39587606211e8cd4
Thomas-Mandon <thomas.mandon@etu.univ-amu.fr>
2019-03-06 15:37:39 +0100

    Update onClick and ContShipBuilder

--- app/src/main/java/com/example/m17006795/boattracker/ContainerShipType.java
+++ app/src/main/java/com/example/m17006795/boattracker/ContainerShipType.java
@@ -2,14 +2,12 @@ package com.example.m17006795.boattracker;
 
 public class ContainerShipType {
     int id;
-    String name;
     int length;
     int height;
     int width;
 
-    public ContainerShipType(int id, String name, int length, int height, int width) {
+    public ContainerShipType(int id, int length, int height, int width) {
         this.id = id;
-        this.name = name;
         this.length = length;
         this.height = height;
         this.width = width;

dacc413c3f5a4d08df1c534d8b2841f606bc268c
JABES Deborah <j17004228@a-maq-iutinfo.salsa.univ-amu.fr>
2019-03-06 15:25:54 +0100

    details

---
+++ app/src/main/java/com/example/m17006795/boattracker/Detailbateau.java
@@ -0,0 +1,13 @@
+package com.example.m17006795.boattracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detailbateau extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailbateau);
+    }
+}

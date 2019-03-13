ec67504ea4abeb8cdee6db80765d290e641e14c2
JABES Deborah <j17004228@a-maq-iutinfo.salsa.univ-amu.fr>
2019-03-13 14:34:18 +0100

    getdistance et attribut bateau dans detailshipactivity

--- app/src/main/java/com/example/m17006795/boattracker/DetailsShipActivity.java
+++ app/src/main/java/com/example/m17006795/boattracker/DetailsShipActivity.java
@@ -1,31 +1,33 @@
 package com.example.m17006795.boattracker;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class DetailsShipActivity extends AppCompatActivity {
 
+    private ContainerShip bateau;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailbateau);
-
+        setContentView(R.layout.activity_detailship);
+        bateau = (ContainerShip) getIntent().getSerializableExtra("Bateau");
         creationDetails();
+        getDistance();
     }
 
     public void creationDetails () {
-        ContainerShip bateau = (ContainerShip) getIntent().getSerializableExtra("Bateau");
+
         ((TextView) findViewById(R.id.shipName)).setText(new StringBuilder("Nom : ").append(bateau.getName()));
         ((TextView) findViewById(R.id.shipType)).setText(new StringBuilder("Type : ").append(bateau.getType().getName()));
     }
 
-    public void calculDist () {
+    public void getDistance() {
+
         Button button = findViewById(R.id.calculDistance);
 
         /*Toast toast = Toast.makeText(DetailsShipActivity.this, , Toast.LENGTH_SHORT);
